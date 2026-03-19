fc75506ab1b342d463c464cff281f6ebc0e4bdd2
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-19 20:14:55 +0200

    chore: add DAO insert

---
+++ StudentsAppSQL9Pro/DAO/IStudentDAO.cs
@@ -0,0 +1,13 @@
+﻿using StudentsAppSQL9Pro.Models;
+
+namespace StudentsAppSQL9Pro.DAO
+{
+    public interface IStudentDAO
+    {
+        Student? Insert(Student student);
+        void Updatea(Student student);
+        void Delete(int id);
+        Student? GetById(int id);
+        List<Student> GetAll();
+    }
+}

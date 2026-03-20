799aa525853c7c65d61e8542b67de7e14c325788
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-20 19:33:59 +0200

    chore: clear dao for student impl

---
+++ StudentsAppSQL9Pro/Services/StudentServiceImpl.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using StudentsAppSQL9Pro.DAO;
+using StudentsAppSQL9Pro.DTO;
+
+namespace StudentsAppSQL9Pro.Services
+{
+    public class StudentServiceImpl : IStudentService
+    {
+        private readonly IStudentDAO _studentDAO;
+        private readonly IMapper _mapper;
+        private readonly ILogger<StudentServiceImpl> _logger; 
+
+        public StudentServiceImpl(IStudentDAO studentDAO, IMapper mapper, ILogger<StudentServiceImpl> logger)
+        {
+            _studentDAO = studentDAO;
+            _mapper = mapper;
+            _logger = logger;
+        }
+
+        public void DeleteStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<StudentReadOnlyDTO> GetAllStudents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentReadOnlyDTO GetStudent(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StudentReadOnlyDTO? InsertStudent(StudentReadOnlyDTO studentInsertDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateStudents(StudentUpdateDTO studentUpdateDTO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

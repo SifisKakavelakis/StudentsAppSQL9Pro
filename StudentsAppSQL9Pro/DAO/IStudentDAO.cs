1ee6929764855bd1bc7da787bcf5e1273a2ddc40
Iosif Kakavelakis <sifiskakavelakis2004@gmail.com>
2026-03-20 18:07:54 +0200

    chore: fixes

--- StudentsAppSQL9Pro/DAO/IStudentDAO.cs
+++ StudentsAppSQL9Pro/DAO/IStudentDAO.cs
@@ -5,7 +5,7 @@ namespace StudentsAppSQL9Pro.DAO
     public interface IStudentDAO
     {
         Student? Insert(Student student);
-        void Updatea(Student student);
+        void Update(Student student);
         void Delete(int id);
         Student? GetById(int id);
         List<Student> GetAll();
---
+++ StudentsAppSQL9Pro/Models/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentsAppSQL9Pro.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+
+    }
+}
